d6270834a79ba1ef524e4b85f29f8d76851024e2
maikel <gmaikel438@gmail.com>
2023-01-02 15:01:01 -0500

    configuracion del views

--- proyecto/peliculas/views.py
+++ proyecto/peliculas/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Director, Pelicula
 
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = 'peliculas/directores.html'
+    model = Director
+
+
+class DirectorView(generic.DetailView):
+    template_name = 'peliculas/director.html'
+    model = Director
+
+
+class PeliculaView(generic.DetailView):
+    template_name = 'peliculas/pelicula.html'
+    model = Pelicula
